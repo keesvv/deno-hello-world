fac53aae3a96d233cc776286bdd5c21388b11438
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2020-12-21 21:42:57 +0100

    chore(deps): add redis

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 export * as oak from 'https://deno.land/x/oak@v6.4.0/mod.ts';
 export * as signal from 'https://deno.land/std@0.81.0/signal/mod.ts';
+export * as redis from 'https://deno.land/x/redis@v0.14.1/mod.ts';
